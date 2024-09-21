e629d5ea4b6181a6de4da4a33294058e74a6ae78
Viviane Rocha <vividerocha@gmail.com>
2024-09-21 11:18:04 -0300

    inclusão da integração com Gemini

---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+import{GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from 'https://cdn.jsdelivr.net/npm/@google/generative-ai/+esm';
+
+const MODEL_NAME = "gemini-1.5-pro-latest";
+const API_KEY = "AIzaSyAGi5xWaHWr_jU0SO6roT7ufE51a1zAdlM";
+
+
+const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    //responseMimeType: "text/plain",
+  };
+
+
+  //BLOQUEIOS DE CONTEÚDOS SENSÍVEIS
+const safetySettings = [
+    {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    }
+]
