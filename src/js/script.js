129053c3ddcc4f45f8d9e67dbf8b2799d1bc04f6
Viviane Rocha <vividerocha@gmail.com>
2024-09-22 10:50:35 -0300

    finalização do projeto integrado com Gemini

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 import{GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from 'https://cdn.jsdelivr.net/npm/@google/generative-ai/+esm';
 
-const MODEL_NAME = "gemini-1.5-pro-latest";
-const API_KEY = "SUA KEY AQUI";
+const MODEL_NAME = 'gemini-1.5-pro-latest';
+const API_KEY = 'AIzaSyCNAWn3ytCsd9LCWgirQES34Qw8DYAZs5A';
 
 
 const generationConfig = {
@@ -32,3 +32,46 @@ const safetySettings = [
         threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
     }
 ]
+
+const button = document.getElementById('botaoSubmit');
+
+button.addEventListener('click', event => {
+    consultaGemini();
+})
+
+async function consultaGemini(){
+    const textInput = document.getElementById('inputText');
+    const textOut = document.getElementById('outText');
+
+    if(textInput.value.length){
+        const texto = textInput.value;
+        limparConsultaTela();
+        const genAi = new GoogleGenerativeAI(API_KEY);
+        const model = genAi.getGenerativeModel({model: MODEL_NAME});
+
+        const chat = model.startChat({
+            generationConfig,
+            safetySettings,
+            history: [],
+        });
+
+        const mensagemChat = `Me responda a pergunta: ${texto} de forma resumida em texto simples e poucas linhas`;
+
+        try {
+            const result = await chat.sendMessage(mensagemChat);
+            const textResult = result.response.text();
+            document.getElementById('outText').innerHTML = textResult;
+          } catch (error) {
+            console.error('Erro na solicitação:', error);
+          }
+
+    }
+
+}
+
+function limparConsultaTela(){
+    const textInput = document.getElementById('inputText');  // Reobter o inputText
+    const textOut = document.getElementById('outText');  // Reobter o textOut
+    textInput.value = "";
+    textOut.innerHTML = "";
+}
